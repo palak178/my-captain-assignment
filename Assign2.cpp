a575820c06817fe1f437b910896701eb8c9a6ced
palak178 <70845267+palak178@users.noreply.github.com>
2020-09-11 00:31:05 +0530

    Update Assign2.cpp

--- Assign2.cpp
+++ Assign2.cpp
@@ -1,12 +1,10 @@
-#include <iostream> 
+#include<iostream>
 using namespace std;
-int main() 
-{ 
-double firstNumber, secondNumber, productOfTwoNumbers; 
-cout << "Enter two numbers: "; 
-// Stores two floating point numbers in variable firstNumber and secondNumber respectively 
-cin >> firstNumber >> secondNumber; 
-// Performs multiplication and stores the result in variable productOfTwoNumbers productOfTwoNumbers = firstNumber * secondNumber;
-cout << "Product = " << productOfTwoNumbers; 
-return 0; 
+in main()
+{
+double a,b;
+cout << "enter 2 numbers to multiply"<< endl;
+cin >> a>> b;
+cout << "product = "  << a*b;
+return 0;
 }
